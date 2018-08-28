5c4219bd9c1ef70fbbacbe0f00ccc42e8407fb28
Quentin Carlin <quentincarlin@Quentins-MacBook-Air.local>
2018-08-28 11:38:28 -0400

    Hoisting

--- 1-grammarAndTypes/04-hoisting.js
+++ 1-grammarAndTypes/04-hoisting.js
@@ -1,5 +1,20 @@
-console.log(scissors)
-var scissors = "yellow scissors"
+ console.log(scissors)
+ var scissors = "yellow scissors"
 
-console.log(cats)
-let cats = the "best"
+// console.log(cats)
+// let cats = "the best"
+
+// console.log(dogs)
+// const dogs = "pretty good"
+
+
+ hiTom()
+ function hiTom(){
+     console.log('Hi Tom!')
+ }
+//  tom()
+ // function expression
+ let tom = function hi(){
+     console.log("hiiii Tom")
+ }
+ tom()
